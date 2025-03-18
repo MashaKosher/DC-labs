a83f7996e104de1c5d8978a9b3ad32ad77209d07
Masha Kosher <kirillsak@icloud.com>
2025-03-18 14:15:54 +0300

    Little fix for Message DB model

--- laba3/models/message.go
+++ laba3/models/message.go
@@ -4,5 +4,5 @@ type Message struct {
 	Country string `json:"Country"`
 	StoryID uint   `json:"storyId"`
 	ID      uint   `json:"id"`
-	Content string `json:content`
+	Content string `json:"content"`
 }
